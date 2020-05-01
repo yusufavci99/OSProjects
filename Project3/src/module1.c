a42e76e781bc9cc8563e03988e13263b0a2ff0eb
yusufavci99 <yusufavci99@gmail.com>
2020-05-02 00:33:36 +0300

    Some work, confused

--- Project3/src/module1.c
+++ Project3/src/module1.c
@@ -10,14 +10,17 @@ extern void nextStep();
 
 int main() {
     // Step 1
+    printf("%s\n\n", "Step 1 Initiated");
+
     printf("%s%lx\n", "Address of main is ", (long unsigned int) main);
     printf("%s%lx\n", "Address of initialized data 1 (128B) is: ", (long unsigned int) initData1);
     printf("%s%lx\n", "Address of unutilized data 1 (16KB) is: ", (long unsigned int) unutilized1);
     printf("%s%lx\n", "Address of initialized data 2 (64B) is: ", (long unsigned int) initData2);
     printf("%s%lx\n", "Address of unutilized data 2 (8KB) is: ", (long unsigned int) unutilized2);
-    printf("%s%lx\n", "Address of printf is: ", (long unsigned int) printf);
+    printf("%s%lx\n", "Address of nextStep function is: ", (long unsigned int) nextStep);
+    printf("%s%lx\n", "Address of printf function is: ", (long unsigned int) printf);
     
-    printf("%d\n", getpid());
+    printf("%s%d\n", "Process PID: ",getpid());
     nextStep();
     // Step 2
 
@@ -28,6 +31,17 @@ int main() {
     printf("%s%lx\n", "Address of data1: ", (long unsigned int) data1);
     printf("%s%lx\n", "Address of data2: ", (long unsigned int) data2);
     printf("%s%lx\n", "Address of data3: ", (long unsigned int) data3);
+    nextStep();
+    printf("%s\n\n", "Step 2 Initiated");
+    nextStep();
+    printf("%s\n\n", "Step 3 Initiated");
+    nextStep();
+    printf("%s\n\n", "Step 4 Initiated");
+    nextStep();
+    printf("%s\n\n", "Step 5 Initiated");
+    nextStep();
+    printf("%s\n\n", "Step 6 Initiated");
+    printf("%s\n", "Enter n to exit");
     nextStep();
     return 0;//
 }
